71fb47a6dadfdb82125d0fa7259525c2a38749d5
Tayte555 <102368286+Tayte555@users.noreply.github.com>
2024-01-23 11:32:09 +0000

    Update Lab01_Java.java

--- Lab Folders/Lab01/Lab01_Java.java
+++ Lab Folders/Lab01/Lab01_Java.java
@@ -4,7 +4,7 @@ void testMethod1()
   {
   //First command x = y + z
   //Second commnad print(x)
-
+  //Edit made by Tayte - 220085397
 
   //Last line print('Good bye')
 }
@@ -21,3 +21,4 @@ void main()
     testMethod1();
     testMethod2();
 }
+
